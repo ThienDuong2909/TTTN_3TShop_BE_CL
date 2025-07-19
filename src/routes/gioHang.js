079bd2aca76c4b7ff8845de95e74222aaf04521b
Thien <thienplpp965@gmail.com>
2025-07-19 20:08:23 +0700

    update: placeOrder

--- src/routes/gioHang.js
+++ src/routes/gioHang.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/them", GioHangController.addToCart);
 router.delete("/xoa", GioHangController.removeFromCart);
 router.post("/dat-hang", GioHangController.placeOrder);
+router.get("/:maKH", GioHangController.getCartByCustomer);
 
 module.exports = router;
